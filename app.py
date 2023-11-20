17f9ab0f4fb6ae76b243733b6549b269cc7cef3f
jaren-ramirez <jarenramirez2019@gmail.com>
2023-11-19 19:31:39 -0900

    multiple graphs can now be put into tabs using callbacks

--- app.py
+++ app.py
@@ -30,20 +30,51 @@ navbar = dbc.NavbarSimple(
 
 app.layout = dbc.Container(children=
     [
-        navbar,  # Inspect this component for any layout constraints
-        dcc.Graph(
-            id="graph", 
-            figure=traceroute_graph.get_plot(),
-        ),
-        dcc.Graph(
-            id="graph2",
-            figure=traceroute_graph.traceroute_hops_graph,
+        navbar,
+        html.Div(id="graph-container", children=[
+          # Inspect this component for any layout constraints
+
+        ]
         )
     ],
     fluid=True,
     style=dict(height="100%", width="100%")  
 )
 
+@app.callback(
+    Output('graph-container', 'children'),
+    [Input('traceroute_button', 'n_clicks'),
+     Input('ping_button', 'n_clicks')]
+)
+def display_content(btn1, btn2):
+    ctx = callback_context
+
+    if not ctx.triggered:
+        return [        dcc.Graph(
+            id="graph", 
+            figure=traceroute_graph.get_plot(),
+        ),
+        dcc.Graph(
+            id="graph2",
+            figure=traceroute_graph.traceroute_hops_graph,
+        )]
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+        if button_id == "traceroute_button":
+            return [        dcc.Graph(
+                id="graph", 
+                figure=traceroute_graph.get_plot(),
+            ),
+            dcc.Graph(
+                id="graph2",
+                figure=traceroute_graph.traceroute_hops_graph,
+            )]
+        elif button_id == "ping_button":
+            return html.Div([
+                html.H3('Content for Button 2'),
+                # Add more content here
+            ])
 
  
 if __name__ == '__main__':
